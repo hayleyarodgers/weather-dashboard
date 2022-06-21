87d979a4c74b5caf52f7b17bf6e379678682b462
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-06-21 15:35:43 +1000

    Add section element variables to JS based on classes and IDs in HTML

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+/* JS DIRECTORY
+    1. =VARIABLES
+    2. =
+    3. =
+*/
+
+/* ===VARIABLES=== */
+
+// Section element variables
+var cityInputEl = $('#city-input');
+var searchButtonEl = $('#search-button');
+var searchHistoryEl = $('#search-history');
+
+var selectedCityNameEl = $('#selected-city-name');
+
+var dateEls = $('.date');
+var iconEls = $('.icon');
+var temperatureEls = $('.temperature');
+var windEls = $('.wind');
+var humidityEls = $('.humidity');
+var uvEl = $('#UV');
